1a08e2cc697b0d727b0c56ba1489aa7793177212
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-01-15 12:53:05 +0000

    added HelloWorld v3.1 (decoupled without interfaces)

---
+++ w02-HelloWorld_Dagger/helloworld_decoupled/src/StandardOutMessageRenderer.java
@@ -0,0 +1,24 @@
+/**
+ * This class is still dependent on the particular implementation HelloWorldMessageProvider:
+ * it should refer to an abstract MessageProvider interface rather than a concrete class.
+ */
+public class StandardOutMessageRenderer {
+    private HelloWorldMessageProvider mp = null;
+
+    public void render() {
+        if(mp == null) {
+            throw new RuntimeException("Message provider not set\n" +
+                    "for class " + StandardOutMessageRenderer.class.getName());
+        }
+        System.out.println(mp.getMessage());
+    }
+
+    public void setMessageProvider(HelloWorldMessageProvider mp) {
+        this.mp = mp;
+    }
+
+    public HelloWorldMessageProvider getMessageProvider() {
+        return mp;
+    }
+
+}
