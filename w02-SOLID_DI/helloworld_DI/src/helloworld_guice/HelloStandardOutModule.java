791bc6f138f955a8c5f11ca8928ede3907c300a8
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-01-19 17:23:25 +0000

    created binding module class

---
+++ w02-SOLID_DI/helloworld_DI/src/helloworld_guice/HelloStandardOutModule.java
@@ -0,0 +1,18 @@
+package helloworld_guice;
+
+import com.google.inject.AbstractModule;
+
+/**
+ *
+ */
+public class HelloStandardOutModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+        bind(MessageProvider.class).to(HelloMessageProvider.class);
+
+        bind(MessageRenderer.class).to(StandardOutMessageRenderer.class);
+
+    }
+}
