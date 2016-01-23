56bc746e5255e63b4215105a8ace6670e47ca87f
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-01-23 11:25:14 +0000

    added main Test class

---
+++ w03_Design-Patterns/src/singleton/Test.java
@@ -0,0 +1,19 @@
+package singleton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Runnable r = new GetSingleton();
+        Thread t1 = new Thread(r, "Thread1");
+        Thread t2 = new Thread(r, "Thread2");
+        t1.start();
+        t2.start();
+    }
+
+}
