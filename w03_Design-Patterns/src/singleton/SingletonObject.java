473a44c6e2910de9668e24e1ecc14cc41c481af7
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-01-23 11:30:37 +0000

    added 'blocking first thread' code and identityHashCode

--- w03_Design-Patterns/src/singleton/SingletonObject.java
+++ w03_Design-Patterns/src/singleton/SingletonObject.java
@@ -1,18 +1,34 @@
 package singleton;
 
 /**
- * Lazy implementation of the Singleton Pattern.
+ * Lazy implementation of the Singleton Pattern, with the first thread blocked for 100ms after entering new init routine;
+ * without synchronization, there's a good chance that two threads will initialise two different instances
+ * of the Singleton.
  */
 public class SingletonObject {
     private static SingletonObject singleton;
+    private static boolean first = true;
 
     private SingletonObject() {
     }
 
     public static SingletonObject getInstance() {
         if(singleton==null) {
+            if (first) {
+                first = false;
+                System.out.println(Thread.currentThread().getName() + " ready to create a new Singleton");
+                try {
+                    System.out.println(Thread.currentThread().getName() + " will sleep a little now...");
+                    Thread.currentThread().sleep(100);
+                } catch (InterruptedException ex) {
+                    // do nothing
+                }
+            }
             singleton = new SingletonObject();
+            System.out.print(Thread.currentThread().getName() + " created instance ");
+            System.out.println(Integer.toHexString(System.identityHashCode(singleton)));
         }
+        System.out.println(Thread.currentThread().getName() + " returning " + Integer.toHexString(System.identityHashCode(singleton)));
         return singleton;
     }
 
