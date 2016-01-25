514c1da49b647878ecd97a898f2baf28d821fa4f
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-01-25 19:03:29 +0000

    added readResolve() method

--- w03_Design-Patterns/src/singleton/SingletonSerializable.java
+++ w03_Design-Patterns/src/singleton/SingletonSerializable.java
@@ -3,8 +3,11 @@ package singleton;
 import java.io.Serializable;
 
 /**
- * Serialization breaks the singleton pattern because when deserialization is invoked
+ * Serialization breaks the Singleton pattern because when deserialization is invoked
  * a new object is created, even if the constructor is private.
+ *
+ * Introducing the readResolve() method, the Singleton instance will be returned after a de-serialization
+ * instead of a new object, so that the Singleton pattern logic is valid.
  */
 public class SingletonSerializable implements Serializable {
     private static SingletonSerializable SINGLETON;
@@ -19,6 +22,17 @@ public class SingletonSerializable implements Serializable {
         }
         return SINGLETON;
     }
+
+    /**
+     * This method is invoked on the new object created by the de-serialization process
+     *
+     * @return the Singleton instance, instead of a different de-serialized object.
+     */
+    private Object readResolve() {
+        return SINGLETON;
+    }
+
+
 
 
 }
