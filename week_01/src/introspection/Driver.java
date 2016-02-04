58c59fd3db8dbc12415adbf72fc4dcf473cf6c8e
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-02-04 03:28:35 +0000

    added answers 1 and 2

--- week_01/src/introspection/Driver.java
+++ week_01/src/introspection/Driver.java
@@ -5,8 +5,18 @@ package introspection;
  */
 public class Driver {
 
+    /**
+     * 1. What are the reasons for using generics here?
+     *    To be able to encapsulate different classes
+     *    into a generic one;
+     * 2. What are the benefits?
+     *    It avoids code repetition (common functionality
+     *    can be called just from Storage, in this case
+     *    getters and setters) and allows more flexibility;
+     */
     public static void main(String[] args) {
-        // YOUR CODE GOES HERE
+        Storage<BankAccount> aStorage = new Storage<>();
+        Storage<String> sStorage = new Storage<>();
     }
 
 }
