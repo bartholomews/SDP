5d2b35af92679d1e20ca039a9dc7aa2c3c7bc558
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-02-12 20:59:21 +0000

    added trait Colour

---
+++ cw-two/src/Colours.scala
@@ -0,0 +1,19 @@
+/**
+  *
+  */
+trait Colours {
+
+  /**
+    * The set of valid colours.
+    */
+  val colours: Set[String] = Set()
+
+  /**
+    * Takes a set of Strings and return the first letter of each, capitalized
+    *
+    * @param strings the set of valid colours
+    * @return the first letter of each, capitalized
+    */
+  def getPegs(strings: Set[String]): Array[Char]
+
+}
