ad52e7d7a63c03439e15552825cb3794d5b3ed03
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-02-15 08:14:11 +0000

    added pattern matching ex_2

--- exercises/week_06/src/forecast/MyForecastTest.scala
+++ exercises/week_06/src/forecast/MyForecastTest.scala
@@ -3,7 +3,7 @@ package forecast
 import com.atomicscala.AtomicTest._
 
 /**
-  * Pattern Matching // 1
+  * Pattern matching
   */
 object MyForecastTest extends App {
   forecast(100) is "Sunny"
@@ -13,6 +13,7 @@ object MyForecastTest extends App {
   forecast(0) is "Cloudy"
   forecast(15) is "Unknown"
 
+  // 1.
   def forecast(temp: Int): String = temp match {
     case 100 => "Sunny"
     case 80 => "Mostly Sunny"
@@ -21,4 +22,13 @@ object MyForecastTest extends App {
     case 0 => "Cloudy"
     case _ => "Unknown"
   }
+
+  println("===================================")
+
+  // 2.
+  val sunnyData = Vector(100, 80, 50, 20, 0, 15)
+  for(i <- sunnyData.indices) {
+    println(forecast(sunnyData(i)))
+  }
+
 }
