879a7bf8c7dbdcb737e7edc1b81fbb247653cc3c
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-02-19 20:51:01 +0000

    cw-two : added impl for game, plus notes on what needs to be done

---
+++ cw-two/src/GameNewImpl.scala
@@ -0,0 +1,22 @@
+/**
+  * Created on 19/02/2016.
+  */
+class GameNewImpl extends GameAbstractImpl {
+
+  @override
+  def runGames: Unit = {
+    do {
+      do {
+        //run a game:
+        //print intro text
+        //generate the code
+        //take the guess (and count guesses)
+        //check the guess against code
+        //print out white/black/etc
+      } while //12 or fewer attempts, then game over //while not correct guess
+      //print game over text
+      //ask if wants new game
+    } while //not quit
+  }
+
+}
