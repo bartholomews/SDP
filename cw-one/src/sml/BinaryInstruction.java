dbab972e1c31c38b04b9e970ef240245149325c4
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-02-24 03:50:08 +0000

    added method toString()

--- cw-one/src/sml/BinaryInstruction.java
+++ cw-one/src/sml/BinaryInstruction.java
@@ -22,7 +22,7 @@ public abstract class BinaryInstruction extends Instruction {
      * to be passed from the implementing subclass to the superclass Instruction
      *
      * @param label the label of the operation
-     * @param op the operation to be performed (particular of the implementing subclass)
+     * @param op    the operation to be performed (particular of the implementing subclass)
      */
     public BinaryInstruction(String label, String op) {
         super(label, op);
@@ -33,8 +33,8 @@ public abstract class BinaryInstruction extends Instruction {
      * Indexes of registers to be used for the Instruction
      *
      * @param result the register which will store the final result
-     * @param op1 the register holding the value of the first operand
-     * @param op2 the register holding the value of the second operand
+     * @param op1    the register holding the value of the first operand
+     * @param op2    the register holding the value of the second operand
      */
     public void setValues(int result, int op1, int op2) {
         this.result = result;
@@ -62,15 +62,17 @@ public abstract class BinaryInstruction extends Instruction {
         return m.getRegisters().getRegister(op2);
     }
 
-    /** 
-     * Gets a String which should be a valid binary operation ("add", "sub", "div" etc.) 
-     * and matches it against {@see BinaryOp} enum class which holds tuples operation/symbol; 
-     * if a valid binary operation (present in BinaryOp list) is passed to the constructor, 
-     * its corresponding enum is passed to the field op. The enum is holding the String value 
-     * of the operation and the associated symbol to be used in toString() method. 
-     * @param op the String op passed to the constructor, representing a valid binary operation; 
-     * @return a {@see BinaryOp} holding the String for that operation an the associated symbol; 
-     * @throws IllegalArgumentException if the String op does not find a match with a BinaryOp value; 
+    /**
+     *
+     * Gets a String which should be a valid binary operation ("add", "sub", "div" etc.)
+     * and matches it against {@see BinaryOp} enum class which holds tuples operation/symbol;
+     * if a valid binary operation (present in BinaryOp list) is passed to the constructor,
+     * its corresponding enum is passed to the field op. The enum is holding the String value
+     * of the operation and the associated symbol to be used in toString() method.
+     *
+     * @param op the String op passed to the constructor, representing a valid binary operation;
+     * @return a {@see BinaryOp} holding the String for that operation an the associated symbol;
+     * @throws IllegalArgumentException if the String op does not find a match with a BinaryOp value;
      */
     public static BinaryOp getOpValueByOpcode(String op) { 
         for (BinaryOp b : BinaryOp.values()) { 
@@ -82,11 +84,23 @@ public abstract class BinaryInstruction extends Instruction {
     }
 
     /**
-     *
      * @return
      */
     public int getResult() {
         return result;
+    }
+
+    /** 
+     * toString() method shared by all the Binary Instructions; 
+     * the particular symbol of the subclass operation is retrieved from the label String in the constructor, 
+     * which is matched against {@see BinaryOp} enum class holding tuples of binary operation/symbol; 
+     * @return a String formed as "label: opcode:" (inherited from superclass Instruction), 
+     *         the indexes of the Registers holding the operands, the symbol of the operation
+     *         to be performed, and the index of the Register which will store the result; 
+     */
+    @Override
+    public String toString() { 
+        return super.toString() + " " + op1 + " " + op.getSymbol() + " " + op2 + " to " + result; 
     }
 
 }
