4948c637d0ea32f7fdd8b5b60c0d891562cf9a58
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-02-24 06:00:07 +0000

    added IntBinaryOperator functionality

--- cw-one/src/sml/BinaryOp.java
+++ cw-one/src/sml/BinaryOp.java
@@ -1,21 +1,32 @@
 package sml;
+import java.util.function.IntBinaryOperator;
 
 /**
+ * Enum class representing a binary operation;
+ * holds the String opcode which should be called from the Machine,
+ * its associated symbol and its associated Integer binary operation
+ * which implementing classes of {@see BinaryInstruction} should use
+ * for the execute(Machine) method.
  *
+ * reference: http://stackoverflow.com/a/28922263
+ *
+ * @author federico.bartolomei
  */
-public enum BinaryOp {
+enum BinaryOp implements IntBinaryOperator {
 
-    ADD("add", '+'),
-    SUB("sub", '-'),
-    MUL("mul", '*'),
-    DIV("div", '/');
+    PLUS("add", '+', (op1, op2) -> op1 + op2),
+    SUB("sub", '-', (op1, op2) -> op1 - op2),
+    MUL("mul", '*', (op1, op2) -> op1 * op2),
+    DIV("div", '/', (op1, op2) -> op1 / op2);
 
-    private char symbol;
-    private String opcode;
+    private final String opcode;
+    private final char symbol;
+    private final IntBinaryOperator op;
 
-    BinaryOp(String label, char symbol) {
+    BinaryOp(final String label, final char symbol, final IntBinaryOperator op) {
         this.opcode = label;
         this.symbol = symbol;
+        this.op = op;
     }
 
     public String getOpcode() {
@@ -25,4 +36,10 @@ public enum BinaryOp {
     public char getSymbol() {
         return symbol;
     }
+
+    @Override
+    public int applyAsInt(final int op1, final int op2) {
+        return op.applyAsInt(op1, op2);
+    }
+
 }
