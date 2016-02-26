d1f958e320a9499c301dbf3bf5346c52ac274876
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-02-26 07:15:42 +0000

    moved under new package

--- week_07/src/traits_inheritance/shape/Draw.scala
+++ week_07/src/traits_inheritance/shape/Draw.scala
@@ -1,4 +1,4 @@
-package traits_inheritance
+package traits_inheritance.shape
 
 /**
   *
--- week_07/src/traits_inheritance/shape/TestColour.scala
+++ week_07/src/traits_inheritance/shape/TestColour.scala
@@ -1,4 +1,4 @@
-package traits_inheritance
+package traits_inheritance.shape
 
 import com.atomicscala.AtomicTest._
 
