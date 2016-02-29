fdb70e02fd6be97077326ad0f84278655901ce83
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-02-29 19:32:02 +0000

    added TODO

--- cw-two/src/ColoursImpl.scala
+++ cw-two/src/ColoursImpl.scala
@@ -3,6 +3,8 @@
   */
 class ColoursImpl(override val colourSet: Set[String]) extends Colours {
 
+  // TODO chars should not have repeating first letters
+
   /**
     * Takes a set of Strings and return the first letter of each, capitalized
     *
