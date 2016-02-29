e0b4cf0676c6333ad9bc60dd3d3f6ca17d3253c9
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-02-29 19:20:01 +0000

    added text, set vals and shuffle

--- cw-two/src/GameNewImpl.scala
+++ cw-two/src/GameNewImpl.scala
@@ -1,22 +1,74 @@
 /**
-  * Created on 19/02/2016.
+  *
   */
-class GameNewImpl extends GameAbstractImpl {
+class GameNewImpl(b: Boolean) extends GameAbstractImpl {
+  // TODO inject these
+  val colours: Colours = new ColoursImpl(Set("Blue", "Yellow", "Red", "Purple", "Green", "Orange"))
+  val shuffler: Shuffler = new ShufflerImpl(4)
+
+  val guesses = 12  // TODO inject?
 
   @Override
   def runGames: Unit = {
+    val won = false
+    //   do {
+    var attemptsLeft = guesses
+    var again = false
+
     do {
+      println("Welcome to Mastermind ...")
+      println("This is a text version of the classic board game Mastermind.")
+      print("The computer will think of a secret code. ")
+      println("The code consists of " + shuffler.length + " colored pegs.")
+      println("The pegs may be one of " + colours.colourSet.size + " colors: " + colours.colourSet.mkString(", "))
+      print("A color may appear more than once in the code.\n" +
+        "You try to guess what colored pegs are in the code and what order they are in.\n" +
+        "After making a guess the result will be displayed.\n" +
+        "A result consists of a black peg for each peg you have exactly correct (color and position) in your guess.\n" +
+        "For each peg in the guess that is the correct color, but is out of position, you get a white peg.\n" +
+        "Only the first letter of the color is displayed. B for Blue, R for Red, and so forth.\n" +
+        "When entering guesses you only need to enter the first character of the color as a capital letter.\n" +
+        "You have " + guesses + " tries to guess the answer or you lose the game.\n")
+
+      println("Generating secret code ....")
+      val pegs: String = shuffler.shuffle(colours.getPegs())
+
       do {
-        //run a game:
+
+        if(b) {
+        println("The secret code is " + pegs)
+        }
+
+        attemptsLeft = attemptsLeft - 1
+        println("You have " + attemptsLeft + " attempts left")
+
+        // game
+
+      } while ((attemptsLeft > 0) && !won)
+
+
+      again = false
+
+    } while(again)
+  }
+}
+          /*
+          blue, green , orange, purple, red, or yellow.
+
+          //run a game:
         //print intro text
         //generate the code
         //take the guess (and count guesses)
         //check the guess against code
         //print out white/black/etc
-      } while //12 or fewer attempts, then game over //while not correct guess
+        finish = true
+      } while(!finish)
+      //12 or fewer attempts, then game over //while not correct guess
       //print game over text
       //ask if wants new game
-    } while //not quit
+ //   } while //not quit
   }
 
 }
+
+*/
