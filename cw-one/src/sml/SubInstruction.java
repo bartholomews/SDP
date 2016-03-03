ba457992c910215060e622afb55edcb6348c154f
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-03-03 06:22:40 +0000

    added implementation and test class

---
+++ cw-one/src/sml/SubInstruction.java
@@ -0,0 +1,43 @@
+package sml;
+
+/**
+ * This class implements the "sub" Instruction: as it is a binary operation
+ * it extends abstract class {@see BinaryInstruction} to inherit its toString()
+ * and functionality of execute(Machine).
+ *
+ * @author federico.bartolomei
+ */
+
+public class SubInstruction extends BinaryInstruction {
+
+    /**
+     * Constructor takes a label which will be passed to superclass
+     * {@see Instruction} together with the opcode retrieved from
+     * {@see BinaryOp}; the indexes of the Registers result, op1 and op2
+     * are stored as fields in {@see BinaryInstruction} to be used
+     * to perform the operation.
+     *
+     * @param label the label of the operation
+     * @param result the index of the Register that will hold the result
+     * @param op1 the index of the Register that hold the first operand
+     * @param op2 the index of the Register that hold the second operand
+     */
+    public SubInstruction(String label, int result, int op1, int op2) {
+        super(label, BinaryOp.SUB.getOpcode(), result, op1, op2);
+    }
+
+    /**
+     * Execute an addition operation on Machine m;
+     * the values of the operands are retrieved by the superclass {@see BinaryInstruction}
+     * using the indexes of the Registers given as arguments in the constructor,
+     * the operation performed is found in {@see BinaryOp} as matched against the value
+     * passed to the superclass at construction time.
+     *
+     * @param m the Machine on which the Instruction is to be performed
+     */
+    @Override
+    public void execute(Machine m) {
+        super.execute(m);
+    }
+
+}
