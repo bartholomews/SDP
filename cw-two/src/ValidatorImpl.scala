6d9c574a1a586eb5736b7e8be35d7806f4bc0a35
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-03-11 21:09:35 +0000

    added functionality to find Whites and Blacks (TODO test)

--- cw-two/src/ValidatorImpl.scala
+++ cw-two/src/ValidatorImpl.scala
@@ -4,22 +4,28 @@
 class ValidatorImpl extends Validator {
 
   override def validateString(s: String, set: Set[Char], length: Int): Boolean = {
-    if(s.length != length) false
+    if(set.isEmpty || s.length != length) false
     else s.forall(c => set.contains(c))
   }
 
-  def findBlacks(): List[(Char, Char)] = {
-    val pegs = List ('A','B','C')
-    val list = List ('A', 'Z', 'Z')
-    val zipped: List[(Char, Char)] = list.zip(pegs)
-
-    zipped.productIterator.foreach{t => println(t)}
-
-
-    return zipped
-
-    //zipped.partition((_,_) =>
-
-
+  def check(input: List[Char], code: List[Char]) = {
+    val (black, white) = partition(input, code)
+    println("Black" * black.length)
+    val inputLeft = white.unzip._1
+    val codeLeft = white.unzip._2
+    codeLeft.foreach(c => {
+      if(checkChar(c, inputLeft)) println ("White")
+    })
   }
+
+  def partition(input: List[Char], code: List[Char]) = {
+    input.zip(code).partition(t => t._1 == t._2)
+  }
+
+  def checkChar(c: Char, l: List[Char]): Boolean = {
+    if(l.isEmpty) false
+    if(c.equals(l.head)) true
+    else checkChar(c, l.drop(l.head))
+  }
+
 }
