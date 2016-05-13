585737f587d25cdbe5ac7dc8077bc6d6e74c705e
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-05-13 18:14:32 +0100

    added exercise 9

--- exercises/week_05/src/further_scala/Counter.scala
+++ exercises/week_05/src/further_scala/Counter.scala
@@ -5,17 +5,24 @@ import com.atomicscala.AtomicTest._
 /**
   * 1.
   */
-class Counter(n: Int) {
+class Counter(val count: Int) {
   // 1.
-  val count = n
-  def inc = new Counter(n + 1)
-  def dec = new Counter(n - 1)
+  def inc = new Counter(count + 1)
+  def dec = new Counter(count - 1)
   // 2.
-  def inc(i: Int = 1) = new Counter(n + i)
-  def dec(d: Int = 1) = new Counter(n - d)
+  def inc(i: Int = 1) = new Counter(count + i)
+  def dec(d: Int = 1) = new Counter(count - d)
   // 3.
-  def adjust(a: Adder, in: Int) = new Counter(a.add(in))
+  def adjust(a: Adder, i: Int) = new Counter(a.add(i))
+}
 
+// 9.
+case class CounterCC(count: Int = 0) {
+  def inc = copy(count + 1)
+  def dec = copy(count -1)
+  def inc(i: Int = 1) = copy(count + i)
+  def dec(i: Int = 1) = copy(count - i)
+  def adjust(a: Adder, i: Int) = copy(a.add(i))
 }
 
 object TestCounter extends App {
@@ -28,4 +35,13 @@ object TestCounter extends App {
   // 3.
   val test3 = new Counter(10).inc.dec.inc(5).adjust(new Adder(5), 2).count
   test3 is 7
+
+  // 9.
+  val test4 = new CounterCC(10).inc.dec.inc.inc.count
+  test4 is 12
+  val test5 = new CounterCC(10).inc(5).dec.dec.dec.dec(2).inc.dec.dec(10).count
+  test5 is 0
+  val test6 = new CounterCC(10).inc.dec.inc(5).adjust(new Adder(5), 2).count
+  test6 is 7
+
 }
